7080f5d48d23516bfd5ed9cea78a5ce5a59bbc62
AStrayker <astrajker@gmail.com>
2025-03-17 12:37:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ cred = credentials.Certificate({
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk%40tgparser-f857c.iam.gserviceaccount.com"
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40tgparser-f857c.iam.gserviceaccount.com"
 })
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://tgparser-f857c-default-rtdb.firebaseio.com'
